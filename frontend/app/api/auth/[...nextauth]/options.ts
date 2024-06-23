761921ce1351adf9cd1a3d93cfa097def849a4e0
Rhythm <65298117+Rhythm1710@users.noreply.github.com>
2024-06-24 03:10:54 +0530

    nextauth tes

--- frontend/app/api/auth/[...nextauth]/options.ts
+++ frontend/app/api/auth/[...nextauth]/options.ts
@@ -1,42 +1,43 @@
-// import GitHubProvider from "next-auth/providers/github"
-// import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
-// export const options = {
-//   providers: [
-//     GitHubProvider({
-//       profile(profile) {
-//         console.log("Profile", profile)
+export const options = {
+  providers: [
+    GitHubProvider({
+      profile(profile) {
+        console.log("Profile", profile)
 
-//         let userRole = "Github User"
-//         if (profile?.email == "itsrhythmarora17@gmail.com") {
-//           userRole = "admin"
-//         }
-//         return {
-//           ...profile,
-//           role: userRole,
-//         }
-//       },
-//       clientId: process.env.GITHUB_ID,
-//       clientSecret: process.env.GITHUB_SECRET,
-//     }),
-//     GoogleProvider({
-//       profile(profile) {
-//         console.log("Profile", profile)
+        let userRole = "Github User"
+        if (profile?.email == "itsrhythmarora17@gmail.com") {
+          userRole = "admin"
+        }
+        return {
+          ...profile,
+          id:profile.id.toString(),
+          role: userRole,
+        }
+      },
+      clientId: getEnvVariable("GOOGLE_ID"),
+      clientSecret: getEnvVariable("GOOGLE_SECRET"),
+    }),
+    GoogleProvider({
+      profile(profile) {
+        console.log("Profile", profile)
 
-//         let userRole = "Github User"
-//         if (profile?.email == "itsrhythmarora17@gmail.com") {
-//           userRole = "admin"
-//         }
-//         return {
-//           ...profile,
-//           id: profile.sub,
-//           role: userRole,
-//         }
-//       },
-//       clientId: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_SECRET,
-//     }),
-//   ],
+        let userRole = "Github User"
+        if (profile?.email == "itsrhythmarora17@gmail.com") {
+          userRole = "admin"
+        }
+        return {
+          ...profile,
+          id: profile.sub,
+          role: userRole,
+        }
+      },
+      clientId: getEnvVariable("GITHUB_ID"),
+      clientSecret: getEnvVariable("GITHUB_SECRET"),
+    }),
+  ],
 //   callbacks: {
 //     async jwt({ token, user }) {
 //       if (user) token.role = user.role
@@ -47,4 +48,8 @@
 //       return session
 //     },
 //   },
-// }
+}
+function getEnvVariable(arg0: string): string {
+    throw new Error("Function not implemented.")
+}
+

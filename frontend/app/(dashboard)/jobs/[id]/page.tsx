9452ae312d4835e971065a9e74df319c63a2b950
Rhythm <65298117+Rhythm1710@users.noreply.github.com>
2024-06-24 04:27:50 +0530

    Trying..

--- frontend/app/(dashboard)/jobs/[id]/page.tsx
+++ frontend/app/(dashboard)/jobs/[id]/page.tsx
@@ -1,24 +1,24 @@
-import EditJobForm from "@/components/EditJobForm"
-import { getSingleJobAction } from "@/utils/actions"
+// import EditJobForm from "@/components/EditJobForm"
+// import { getSingleJobAction } from "@/utils/actions"
 
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query"
+// import {
+//   dehydrate,
+//   HydrationBoundary,
+//   QueryClient,
+// } from "@tanstack/react-query"
 
-async function JobDetailPage({ params }: { params: { id: string } }) {
-  const queryClient = new QueryClient()
+// async function JobDetailPage({ params }: { params: { id: string } }) {
+//   const queryClient = new QueryClient()
 
-  await queryClient.prefetchQuery({
-    queryKey: ["job", params.id],
-    queryFn: () => getSingleJobAction(params.id),
-  })
+//   await queryClient.prefetchQuery({
+//     queryKey: ["job", params.id],
+//     queryFn: () => getSingleJobAction(params.id),
+//   })
 
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <EditJobForm jobId={params.id} />
-    </HydrationBoundary>
-  )
-}
-export default JobDetailPage
+//   return (
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       <EditJobForm jobId={params.id} />
+//     </HydrationBoundary>
+//   )
+// }
+// export default JobDetailPage
